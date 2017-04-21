df611ae8ca8873b2f3086727669e9bf3ca2e418a
Iain Merrick <iain@iainmerrick.com>
2017-04-21 15:28:08 -0500

    Very basic build set with tup / browserify / uglifyjs

---
+++ src/hello.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function Hello(person) {
+    return "Hello, " + person;
+}
+
+exports.Hello = Hello;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var hello = require("./hello");
+
+document.body.innerHTML = hello.Hello("world");
