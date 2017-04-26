c6ef7e107bec64247c6f8f2eb08a77c278d76764
Iain Merrick <iain@iainmerrick.com>
2017-04-26 00:47:21 -0500

    Simple get_seats function

--- src/election.js
+++ src/election.js
@@ -1,7 +1,29 @@
 "use strict";
 
-function run(data) {
-    console.log("Running election: " + data);
+function get_seats(votes) {
+    var keys = votes[0];
+    var seats = []
+    var i, j;
+    for (i = 1; i < keys.length; i++) {
+        seats[i] = 0;
+    }
+    for (i = 1; i < votes.length; i++) {
+        var row = votes[i];
+        var max_index = 0;
+        var max_value = 0;
+        for (j = 1; j < row.length; j++) {
+            if (row[j] > max_value) {
+                max_index = j;
+                max_value = row[j];
+            }
+        }
+        seats[max_index] += 1;
+    }
+    var seat_map = {};
+    for (i = 1; i < keys.length; i++) {
+        seat_map[keys[i]] = seats[i];
+    }
+    return seat_map;
 }
 
-exports.run = run;
+exports.get_seats = get_seats;
--- test/election_test.js
+++ test/election_test.js
@@ -10,19 +10,21 @@ const election = require("../src/election");
 // The first row contains column headers (e.g. party names).
 // The remaining rows contain voting data, one row per constituency.
 // The first column is assumed to be the constituency name.
-const DATA = [
+const VOTES = [
     ["Name",                "Conservative", "Labour",   "Liberal Democrat", "SNP"],
     ["Scotland",            3000,           5000,       2000,               8000],
     ["England",             25000,          20000,      15000,              0],
-    ["Wales",               2000,           3000,       4000,               0],
+    ["Wales",               2000,           4000,       3000,               0],
     ["Northern Ireland",    500,            300,        100,                0]
 ];
 
-const RESULT = {
-    "Conservative": 1,
+const SEATS = {
+    "Conservative": 2,
     "Labour": 1,
-    "Liberal Democrat": 1,
+    "Liberal Democrat": 0,
     "SNP": 1
 };
 
-assert.deepStrictEqual(election.run(DATA), RESULT);
+assert.deepStrictEqual(election.get_seats(VOTES), SEATS);
+
+console.log("Success!");
