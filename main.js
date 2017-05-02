f614db65d8fbab700462d2bb7f15186e6d7c939a
Iain Merrick <iain@iainmerrick.com>
2017-05-02 16:31:32 -0500

    Starting to get vaguely declarative

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import * as election from "./src/election.js";
 import * as party from "./src/party.js";
 import * as swingometer from "./src/swingometer.js";
 
-function loadChart(json, bar_ctx, pie_ctx) {
+function loadChart(json, div) {
     let seat_map = election.get_seats(json);
     let vote_map = election.get_votes(json);
     vote_map = swingometer.normalize_votes(vote_map);
@@ -26,85 +26,90 @@ function loadChart(json, bar_ctx, pie_ctx) {
     let total_seats = json.length - 1;
     let max_percent = _.max([_.max(votes), _.max(seats) / total_seats]);
 
-    let bar = new Chart(bar_ctx, {
-        type: "bar",
-        options: {
-            legend: {
-                display: false
-            },
-            scales: {
-                yAxes: [{
-                    id: "votes",
-                    position: "left",
-                    ticks: {
-                        beginAtZero: true,
-                        suggestedMax: max_percent,
-                        callback: function(value) {
-                            return (value * 100).toFixed(0) + "%";
+    div.find(".bar").each(function(ix, element) {
+        new Chart(element, {
+            type: "bar",
+            options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        id: "votes",
+                        position: "left",
+                        ticks: {
+                            beginAtZero: true,
+                            suggestedMax: max_percent,
+                            callback: function(value) {
+                                return (value * 100).toFixed(0) + "%";
+                            }
+                        },
+                        gridLines: {
+                            drawOnChartArea: false
+                        },
+                        scaleLabel: {
+                            labelString: "Votes"
                         }
-                    },
-                    gridLines: {
-                        drawOnChartArea: false
-                    },
-                    scaleLabel: {
-                        labelString: "Votes"
-                    }
+                    }, {
+                        id: "seats",
+                        position: "right",
+                        ticks: {
+                            beginAtZero: true,
+                            suggestedMax: max_percent * total_seats
+                        },
+                        scaleLabel: {
+                            labelString: "Seats"
+                        }
+                    }]
+                }
+            },
+            data: {
+                labels: names,
+                datasets: [{
+                    label: "Votes",
+                    yAxisID: "votes",
+                    data: votes,
+                    backgroundColor: colors,
+                    borderWidth: 1
                 }, {
-                    id: "seats",
-                    position: "right",
-                    ticks: {
-                        beginAtZero: true,
-                        suggestedMax: max_percent * total_seats
-                    },
-                    scaleLabel: {
-                        labelString: "Seats"
-                    }
+                    label: "Seats",
+                    yAxisID: "seats",
+                    data: seats,
+                    backgroundColor: colors,
+                    borderWidth: 1
                 }]
             }
-        },
-        data: {
-            labels: names,
-            datasets: [{
-                label: "Votes",
-                yAxisID: "votes",
-                data: votes,
-                backgroundColor: colors,
-                borderWidth: 1
-            }, {
-                label: "Seats",
-                yAxisID: "seats",
-                data: seats,
-                backgroundColor: colors,
-                borderWidth: 1
-            }]
-        }
+        });
     });
 
-    let pie = new Chart(pie_ctx, {
-        type: "doughnut",
-        options: {
-            legend: {
-                display: false
+    div.find(".pie").each(function(ix, element) {
+        let pie = new Chart(element, {
+            type: "doughnut",
+            options: {
+                legend: {
+                    display: false
+                },
             },
-        },
-        data: {
-            labels: names,
-            datasets: [{
-                label: "Seats",
-                data: seats,
-                backgroundColor: colors,
-                borderWidth: 0
-            }]
-        }
+            data: {
+                labels: names,
+                datasets: [{
+                    label: "Seats",
+                    data: seats,
+                    backgroundColor: colors,
+                    borderWidth: 0
+                }]
+            }
+        });
     });
 }
 
 $.getJSON("election_2010.json", function(json) {
     console.log("Loaded 2010 data - " + json.length + " rows");
-    loadChart(json, document.getElementById("bar2010"), document.getElementById("pie2010"));
+    loadChart(json, $("#election2010"));
 });
 
 $.getJSON("election_2015.json", function(json) {
     console.log("Loaded 2015 data - " + json.length + " rows");
-    loadChart(json, document.getElementById("bar2015"), document.getElementById("pie2015"));
+    let div = document.getElementById("election2015");
+    loadChart(json, $("#election2015"));
 });
