3f2264c14c43a0d2403de2cfcffab5827db7c1b6
Iain Merrick <iain@iainmerrick.com>
2017-05-02 10:35:08 -0500

    Wow, jquery's map() function is awful. Use underscore instead

--- main.js
+++ main.js
@@ -1,50 +1,39 @@
 "use strict";
 
+import _ from "underscore";
 import $ from "jquery";
 import "chart.js";
 
-var ctx = document.getElementById("myChart");
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+import * as election from "./src/election.js";
+
+function loadChart(ctx, json) {
+    let seats = election.get_seats(json);
+    let pairs = _.pairs(seats);
+    // Sort by name, then by descending seat count (preserving name order in ties)
+    pairs = _.sortBy(pairs, function(pair) { return pair[0]; });
+    pairs = _.sortBy(pairs, function(pair) { return -pair[1]; });
+    let keys = _.map(pairs, function(pair) { return pair[0]; });
+    let values = _.map(pairs, function(pair) { return pair[1]; });
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: keys,
+            datasets: [{
+                label: "Seats",
+                data: values
             }]
         }
-    }
-});
+    });
+}
 
 $.getJSON("election_2010.json", function(json) {
     console.log("Loaded 2010 data - " + json.length + " rows");
+    let ctx = document.getElementById("chart2010");
+    loadChart(ctx, json);
 });
 
 $.getJSON("election_2015.json", function(json) {
     console.log("Loaded 2015 data - " + json.length + " rows");
+    let ctx = document.getElementById("chart2015");
+    loadChart(ctx, json);
 });
