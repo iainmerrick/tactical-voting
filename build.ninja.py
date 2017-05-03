cdc38fba90117aaffc655df354ddbd84604b4800
Iain Merrick <iain@iainmerrick.com>
2017-05-03 14:06:18 -0500

    Add html-minifier

--- build.ninja.py
+++ build.ninja.py
@@ -13,10 +13,14 @@ SRCS = glob.glob("src/*.js")
 print """
 NODE_BIN = node_modules/.bin
 
-BROWSERIFY = ${NODE_BIN}/browserify
-UGLIFYJS = ${NODE_BIN}/uglifyjs
 BABEL = ${NODE_BIN}/babel
+BROWSERIFY = ${NODE_BIN}/browserify
+HTML_MINIFY = ${NODE_BIN}/html-minifier
+UGLIFYJS = ${NODE_BIN}/uglifyjs
 CSV_TO_JSON = data/election_csv_to_json.js
+
+UGLIFYJS_FLAGS = --compress --mangle
+HTML_MINIFY_FLAGS = --collapse-whitespace --remove-comments --remove-optional-tags
 
 NODE = .package.json.stamp
 
@@ -36,10 +40,13 @@ rule bundle
 # The 'cd' is a stupid hack to make uglify generate the correct source map path.
 # I'm sure there's a command-line flag that'll do this but life is short (as are the docs)
 rule ugly
-    command = cd `dirname $in` && ../$UGLIFYJS `basename $in` -o ../$out --in-source-map inline --source-map ../$out.map --source-map-includeSources --compress --mangle
+    command = cd `dirname $in` && ../$UGLIFYJS `basename $in` -o ../$out --in-source-map inline --source-map ../$out.map --source-map-includeSources  $UGLIFYJS_FLAGS
 
 rule csv_to_json
     command = $CSV_TO_JSON $in > $out
+
+rule html_minify
+    command = $HTML_MINIFY $HTML_MINIFY_FLAGS $in > $out
 
 rule copy
     command = cp $in $out
@@ -60,5 +67,5 @@ build out/bundle.js : bundle out/src/main.js | %(OUT_SRCS)s $NODE
 build docs/main.js | docs/main.js.map: ugly out/bundle.js | $NODE
 build docs/election_2010.json : csv_to_json data/election_2010.csv | $NODE %(OUT_SRCS)s
 build docs/election_2015.json : csv_to_json data/election_2015.csv | $NODE %(OUT_SRCS)s
-build docs/index.html : copy src/index.html
+build docs/index.html : html_minify src/index.html
 """ % locals()
