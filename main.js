815e9061564b8ef334b9c18b8104d7cad9bfe281
Iain Merrick <iain@iainmerrick.com>
2017-05-03 12:17:17 -0500

    Interactive charts, finally

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ import "chart.js";
 import * as election from "./src/election.js";
 import * as party from "./src/party.js";
 import * as swingometer from "./src/swingometer.js";
+import * as tactics from "./src/tactics.js";
 
-let NAMES = [
+const NAMES = [
     "Con",
     "Lab",
     "LD",
@@ -18,107 +19,148 @@ let NAMES = [
     "Other"
 ];
 
-let ZEROES = _.map(NAMES, function(name) { return 0; });
-let COLORS = _.map(NAMES, function(name) { return party.color(name); });
+const ZEROES = _.map(NAMES, function(name) { return 0; });
+const COLORS = _.map(NAMES, function(name) { return party.color(name); });
 
-function createCharts(div) {
-    var charts = {
-        votes: [],
-        seats: []
-    };
+class ElectionView {
 
-    div.find(".votes").each(function(ix, element) {
-        let chart = new Chart(element, {
-            type: "bar",
-            options: {
-                legend: {
-                    display: false
-                },
-            },
-            data: {
-                labels: NAMES,
-                datasets: [{
-                    label: "Votes",
-                    data: ZEROES.slice(),
-                    backgroundColor: COLORS
-                }]
-            }
+    constructor(div, json) {
+        this.div = div;
+        this.json = json;
+        this.checkbox_map = {};
+        this.vote_charts = [];
+        this.seat_charts = [];
+
+        let view = this;
+
+        div.find("input:checkbox").each(function(ix, checkbox) {
+            view.checkbox_map[checkbox.name] = checkbox;
         });
-        charts.votes.push(chart);
-    });
-
-    div.find(".seats").each(function(ix, element) {
-        let chart = new Chart(element, {
-            type: "bar",
-            options: {
-                legend: {
-                    display: false
-                },
-            },
-            data: {
-                labels: NAMES,
-                datasets: [{
-                    label: "Seats",
-                    data: ZEROES.slice(),
-                    backgroundColor: COLORS
-                }]
-            }
+        div.find("input:checkbox").change(function(element) {
+            console.log("Clicked checkbox: " + element.name);
+            view.update();
         });
-        charts.seats.push(chart);
-    });
 
-    div.find(".pie").each(function(ix, element) {
-        let chart = new Chart(element, {
-            type: "doughnut",
-            options: {
-                legend: {
-                    display: false
+        div.find("canvas.votes").each(function(ix, canvas) {
+            let chart = new Chart(canvas, {
+                type: "bar",
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                max: 0.5
+                            }
+                        }]
+                    }
                 },
-            },
-            data: {
-                labels: NAMES,
-                datasets: [{
-                    label: "Seats",
-                    borderWidth: 0,
-                    data: ZEROES.slice(),
-                    backgroundColor: COLORS
-                }]
-            }
+                data: {
+                    labels: NAMES,
+                    datasets: [{
+                        label: "Votes",
+                        data: ZEROES.slice(),
+                        backgroundColor: COLORS
+                    }]
+                }
+            });
+            view.vote_charts.push(chart);
         });
-        charts.seats.push(chart);
-    });
-    
-    return charts;
-}
 
-function refreshCharts(charts, json) {
-    let seat_map = election.get_seats(json);
-    let vote_map = election.get_votes(json);
-    vote_map = swingometer.normalize_votes(vote_map);
+        div.find("canvas.seats").each(function(ix, canvas) {
+            let chart = new Chart(canvas, {
+                type: "bar",
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                max: 400
+                            }
+                        }]
+                    }
+                },
+                data: {
+                    labels: NAMES,
+                    datasets: [{
+                        label: "Seats",
+                        data: ZEROES.slice(),
+                        backgroundColor: COLORS
+                    }]
+                }
+            });
+            view.seat_charts.push(chart);
+        });
 
-    for (let votes of charts.votes) {
-        for (let i = 0; i < NAMES.length; ++i) {
-            votes.data.datasets[0].data[i] = vote_map[NAMES[i]];
-        }
-        votes.update();
+        div.find("canvas.pie").each(function(ix, canvas) {
+            let chart = new Chart(canvas, {
+                type: "doughnut",
+                options: {
+                    legend: {
+                        display: false
+                    },
+                },
+                data: {
+                    labels: NAMES,
+                    datasets: [{
+                        label: "Seats",
+                        borderWidth: 0,
+                        data: ZEROES.slice(),
+                        backgroundColor: COLORS
+                    }]
+                }
+            });
+            view.seat_charts.push(chart);
+        });
+
+        this.update();
     }
-    for (let seats of charts.seats) {
-        for (let i = 0; i < NAMES.length; ++i) {
-            seats.data.datasets[0].data[i] = seat_map[NAMES[i]];
+
+    update() {
+        let bloc = [];
+        for (let party in this.checkbox_map) {
+            let checkbox = this.checkbox_map[party];
+            if (checkbox.checked) {
+                bloc.push(party);
+            }
         }
-        seats.update();
+
+        console.log("Voting bloc: " + bloc);
+        let data = tactics.adjust_data_with_tactics(this.json, bloc);
+        
+        let seat_map = election.get_seats(data);
+        console.log(seat_map);
+        let vote_map = election.get_votes(data);
+        vote_map = swingometer.normalize_votes(vote_map);
+
+        for (let votes of this.vote_charts) {
+            for (let i = 0; i < NAMES.length; ++i) {
+                votes.data.datasets[0].data[i] = vote_map[NAMES[i]];
+            }
+            votes.update();
+        }
+        for (let seats of this.seat_charts) {
+            for (let i = 0; i < NAMES.length; ++i) {
+                seats.data.datasets[0].data[i] = seat_map[NAMES[i]];
+            }
+            seats.update();
+        }
     }
 }
-
-let election2010 = createCharts($("#election2010"));
-let election2015 = createCharts($("#election2015"));
 
 $.getJSON("election_2010.json", function(json) {
     console.log("Loaded 2010 data - " + json.length + " rows");
-    refreshCharts(election2010, json);
+    let div = $("#election2010");
+    new ElectionView(div, json);
 });
 
 $.getJSON("election_2015.json", function(json) {
     console.log("Loaded 2015 data - " + json.length + " rows");
-    refreshCharts(election2015, json);
+    let div = $("#election2015");
+    new ElectionView(div, json);
 });
